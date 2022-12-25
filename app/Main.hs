e0ca80d0e229247a3bb0efdc6443ccf2b89fc59f
Javier Arrieta <javierarrieta@google.com>
2022-12-25 19:50:26 +0100

    Refactor validation

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,24 @@
 module Main where
 
 import Data.Validation
-import Data.List.NonEmpty
+import Control.Arrow (left)
+import Flow
 
-romanChar2Int :: Char -> Validation [String] Int
-romanChar2Int 'I' = Success 1
-romanChar2Int 'V' = Success 5
-romanChar2Int 'X' = Success 10
-romanChar2Int 'L' = Success 50
-romanChar2Int 'C' = Success 100
-romanChar2Int 'D' = Success 500
-romanChar2Int 'M' = Success 1000
-romanChar2Int e = Failure [('\'' : e : "' is not a valid Roman character")]
+romanChar2Int :: Char -> Either String Int
+romanChar2Int 'I' = Right 1
+romanChar2Int 'V' = Right 5
+romanChar2Int 'X' = Right 10
+romanChar2Int 'L' = Right 50
+romanChar2Int 'C' = Right 100
+romanChar2Int 'D' = Right 500
+romanChar2Int 'M' = Right 1000
+romanChar2Int e = Left ('\'' : e : "' is not a valid Roman character")
+
+either2Validated :: Either String Int -> Validation [String] Int
+either2Validated e = fromEither (left (:[]) e)
 
 roman2IntValidated :: [Char] -> Validation [String] [Int]
-roman2IntValidated l = traverse romanChar2Int l
+roman2IntValidated l = traverse (romanChar2Int .> either2Validated) l
 
 main = do
   print (romanChar2Int 'C')
