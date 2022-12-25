45f5a5d4ed7e7a3c8b35a1c4ca9f6e78f1341b61
Javier Arrieta <javierarrieta@google.com>
2022-12-25 20:11:40 +0100

    Remove white spaces

--- app/Main.hs
+++ app/Main.hs
@@ -14,14 +14,14 @@ romanChar2Int 'D' = Right 500
 romanChar2Int 'M' = Right 1000
 romanChar2Int e = Left ('\'' : e : "' is not a valid Roman character")
 
-either2Validated :: Either String Int -> Validation [String] Int
+either2Validated :: Either a b -> Validation [a] b
 either2Validated e = fromEither (left (:[]) e)
 
 roman2IntValidated :: [Char] -> Validation [String] [Int]
 roman2IntValidated l = traverse (romanChar2Int .> either2Validated) l
 
 main = do
-  print (romanChar2Int 'C')
-  print (romanChar2Int 'J')
+  print(romanChar2Int 'C')
+  print(romanChar2Int 'J')
   print(roman2IntValidated "ABC")
   print(roman2IntValidated "MMXXII")
